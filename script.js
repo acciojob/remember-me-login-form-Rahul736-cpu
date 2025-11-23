fa0a05fd7047057d9be8b58cb682965813f0a03a
Accio-1 <rahulwanjare291@gmail.com>
2025-11-23 11:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+window.onload = function () {
+    const existingBtn = document.getElementById("existing");
+    const savedUser = localStorage.getItem("username");
+    const savedPass = localStorage.getItem("password");
+
+    if (savedUser && savedPass) {
+        existingBtn.style.display = "block";
+    }
+
+    existingBtn.addEventListener("click", () => {
+        alert("Logged in as " + savedUser);
+    });
+
+    document.getElementById("loginForm").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        const remember = document.getElementById("checkbox").checked;
+
+        alert("Logged in as " + username);
+
+        if (remember) {
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+        } else {
+            localStorage.removeItem("username");
+            localStorage.removeItem("password");
+        }
+
+        if (localStorage.getItem("username")) {
+            existingBtn.style.display = "block";
+        } else {
+            existingBtn.style.display = "none";
+        }
+    });
+};
